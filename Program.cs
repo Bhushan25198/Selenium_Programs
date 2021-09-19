9555f474c9bea4722b5363b9dc43bbbe2441c913
Bhushan25198 <90633313+Bhushan25198@users.noreply.github.com>
2021-09-19 21:21:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chrome_Lounch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+            IWebDriver driver = new ChromeDriver("C:\\SeleniumProjects\\chromedriver_win32");
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://www.google.co.in/";
+
+            //driver.Close();
+        }
+    }
+}
