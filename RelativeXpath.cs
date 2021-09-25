663b28ac929bc8e994375f08a2d842fb6ad3c3ad
Bhushan25198 <90633313+Bhushan25198@users.noreply.github.com>
2021-09-25 11:00:24 +0530

    Add files via upload

---
+++ RelativeXpath.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relative_Xpath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+            IWebDriver driver = new ChromeDriver("C:\\SeleniumProjects\\chromedriver_win32");
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://www.google.co.in/";
+
+            driver.FindElement(By.XPath("//input[@role='combobox']")).SendKeys("Selenium");
+
+
+            
+            //driver.Close();
+        }
+    }
+}
