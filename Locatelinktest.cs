e5941a64e5522452cca740fcbb5b30902d95a4f7
Bhushan25198 <90633313+Bhushan25198@users.noreply.github.com>
2021-09-25 11:01:40 +0530

    Add files via upload

---
+++ Locatelinktest.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locate_By_LinkText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+            IWebDriver driver = new ChromeDriver("C:\\SeleniumProjects\\chromedriver_win32");
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://www.google.co.in/";
+
+           String gmailText= driver.FindElement(By.LinkText("Gmail")).Text;
+
+            Console.WriteLine(gmailText);
+
+            driver.Close();
+        }
+    }
+}
