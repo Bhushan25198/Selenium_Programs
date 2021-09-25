905a43a91bf5bac2c98c693a3d912042f98c1411
Bhushan25198 <90633313+Bhushan25198@users.noreply.github.com>
2021-09-25 11:03:28 +0530

    Add files via upload

---
+++ Cssselector.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSSSelectorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+
+            IWebDriver driver = new ChromeDriver("C:\\SeleniumProjects\\chromedriver_win32");
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://www.google.co.in/";
+            driver.FindElement(By.CssSelector("input[role$='box']")).SendKeys("Selenium");
+
+            Thread.Sleep(3000);
+
+            driver.Close();
+
+        }
+    }
+}
